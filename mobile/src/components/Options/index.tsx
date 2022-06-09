ae891ec6b8e5acbee9a847a691e760bcfc9edee8
Kauê de Magalhães <61990018+kkkaue@users.noreply.github.com>
2022-06-09 17:55:13 -0300

    add mobile

---
+++ mobile/src/components/Options/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+import { Copyright } from '../Copyright';
+import { Option } from '../Option';
+import { FeedbackType } from '../Widget';
+
+import { feedbackTypes } from '../../utils/feedbackTypes'
+import { styles } from './styles';
+
+interface Props {
+  onFeedbackTypeChange: (feedbackType: FeedbackType) => void
+}
+
+export function Options({onFeedbackTypeChange}: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Deixe seu feedback
+      </Text>
+
+      <View style={styles.options}>
+        {
+          Object
+          .entries(feedbackTypes)
+          .map(([key, value]) => (
+            <Option
+              key={key}
+              title={value.title}
+              image={value.image}
+              onPress={() => onFeedbackTypeChange(key as FeedbackType)}
+            />
+          ))
+        }
+      </View>
+      <Copyright />
+    </View>
+  );
+}
